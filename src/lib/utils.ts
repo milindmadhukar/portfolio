3b17ce9d8a4fb9948ede0987cd81c9065f4e5bc1
PyBash <67195650+pybash1@users.noreply.github.com>
2025-12-07 13:07:17 +0530

    setup and basics

---
+++ src/lib/utils.ts
@@ -0,0 +1,4 @@
+export const getUptime = (dob: Date) => {
+    const now = new Date();
+    return `${Math.round((now.getTime() - dob.getTime()) / 31556952000)} years, ${Math.ceil((now.getTime() - dob.getTime()) % 31556952000 / 2629746000)} months, ${Math.round((now.getTime() - dob.getTime()) % 31556952000 % 2629746000 / 86400000)} days`;
+}
